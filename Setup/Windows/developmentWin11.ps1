cf33882b262da7567d0a8d21ebad2a59114f5107
vomicz <t.vomacka@seznam.cz>
2023-07-11 13:10:55 -0700

    f added quality of life stuff

---
+++ Setup/Windows/developmentWin11.ps1
@@ -0,0 +1,22 @@
+# If this doesn't run, try setting the execution policy by: set-executionpolicy remotesigned
+
+#Requires -RunAsAdministrator
+
+# Check if Chocolatey is already installed
+if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
+    # Install Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    Write-Output "Chocolatey is already installed."
+}
+
+choco feature enable --name=allowGlobalConfirmation
+
+#Quality of life improvements
+Remove-Item -ErrorAction SilentlyContinue -Recurse HKCU:\AppEvents\Schemes
+Set-Service Audiosrv -StartupType Automatic
+
+#General tools
+
+#Development
